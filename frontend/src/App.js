6efda9b276c3e8c42db2fbf90114ab7851ac5b59
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-01-02 15:15:17 +0600

    creact-react-app-for-building-amazona-website

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">amazone</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
