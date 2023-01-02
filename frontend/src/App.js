96c0713e1a062c100d7379fc852c73531380c146
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-01-02 21:22:08 +0600

    Addpage-route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href="/">amazone</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-              <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p><strong>{product.price}</strong></p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Link to="/">amazone</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path='/product/:slug' element={<ProductScreen />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
