e6da853ec06b9b9a0d4b4adad39f5b9f743086f1
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-01-07 14:09:21 +0600

    Connect-to-mongoDB-Database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import data from './data.js';
-
+import dotenv from 'dotenv' 
+import mongoose from 'mongoose';
+   
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('connected to db')
+}).catch((err) => { 
+  console.log(err.message) 
+})    
+           
 const app = express();
 
 app.get('/api/products', (req, res) => {
