0eba0e40f37147a2d16851550d91d0185099045b
dm-abu-hasib <dmabuhasib362000@gmail.com>
2023-01-12 02:20:59 +0600

    Publish-To-Render

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path'
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import mongoose from 'mongoose';
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
-dotenv.config();
+dotenv.config(); 
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -30,11 +30,14 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+const _dirname = path.resolve()
+app.use(express.static(path.join(_dirname, '/frontend/build')))
+app.get('*', (req, res) => res.sendFile(path.join(_dirname, '/frontend/build/index.html')))
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
